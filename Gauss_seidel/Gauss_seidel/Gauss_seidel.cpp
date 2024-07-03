207a30f80919ba6bc90d7b1973b192d4c58a95c5
AlinaKonshina <konshina.alina@mail.ru>
2024-07-03 20:07:32 +0300

    development
    Prog2

---
+++ Gauss_seidel/Gauss_seidel/Gauss_seidel.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+int main()
+{
+	double t = 0.0, h, tau;
+	int N = 10, M;
+	M = 2 * N * N;
+	tau = 1.0 / M;
+	h = 1.0 / N;
+	double pi = 2 * asin(1);
+	double tk = 5.0;
+	double Norma = 0.0;
+	double NORMA = 1.0;
+	int c = 1;
+	int q = 0;
+	double x = 0.0;
+	int nomer = 0;
+
+	double** T1 = new double* [N + 1];
+	for (int i = 0; i < N + 1; i++) {
+		T1[i] = new double[N + 1];
+	}
+	double** T2 = new double* [N + 1];
+	for (int i = 0; i < N + 1; i++) {
+		T2[i] = new double[N + 1];
+	}
+	double** T3 = new double* [N + 1];
+	for (int i = 0; i < N + 1; i++) {
+		T3[i] = new double[N + 1];
+	}
+	double** T4 = new double* [N + 1];
+
+	ofstream file;
+	file.open("res1.txt");
+
+	for (int i = 0; i < N + 1; i++) {
+		for (int j = 0; j < N + 1; j++) {
+			T1[i][j] = sin(pi * i * h * j * h * 0.5);
+		}
+	}//заполняем первый слой в свободном виде
+
+	for (int i = 0; i < N + 1; i++) {
+		for (int j = 0; j < N + 1; j++) {
+			file << setw(10) << T1[i][j] << " ";
+		} file << endl;
+	}//вывод в файл первого слоя
+
+	while ((tau * c) < tk) {
+
+		while (NORMA > 0.001) {
+
+			for (int ii = 0; ii < N + 1; ii++) {
+				T2[0][ii] = 0.0;
+				T2[N][ii] = 1.0;
+			}
+			for (int ii = 1; ii < N; ii++) {
+				T2[ii][0] = 0.0;
+				T2[ii][N] = 1.0;
+			}//заполняем границы
+
+			if (q == 0) {
+				for (int i = 1; i < N; i++) {
+					for (int j = 1; j < N; j++) {
+						T2[i][j] = (T1[i][j] / tau + (T1[i + 1][j] + T2[i - 1][j] + T1[i][j + 1] + T2[i][j - 1]) / (h * h)) / (1 / tau + 4 / (h * h));
+					}
+				}
+
+				q++;
+			}
+			else {
+				for (int i = 1; i < N; i++) {
+					for (int j = 1; j < N; j++) {
+						T2[i][j] = (T1[i][j] / tau + (T3[i + 1][j] + T2[i - 1][j] + T3[i][j + 1] + T2[i][j - 1]) / (h * h)) / (1 / tau + 4 / (h * h));
+					}
+				}
+			}
+			for (int i = 0; i < N + 1; i++) {
+				for (int j = 0; j < N + 1; j++) {
+					if (i == 0 || j == 0 || i == N || j == N) {
+						Norma += 0.0;
+					}
+					else {
+						Norma += ((T2[i][j] - T1[i][j]) / tau - (T2[i + 1][j] + T2[i - 1][j] + T2[i][j + 1] + T2[i][j - 1] - 4 * T2[i][j]) / (h * h)) * ((T2[i][j] - T1[i][j]) / tau - (T2[i + 1][j] + T2[i - 1][j] + T2[i][j + 1] + T2[i][j - 1] - 4 * T2[i][j]) / (h * h));
+					}
+				}
+			}
+
+			NORMA = sqrt(Norma);
+
+			//Вывод невязки и номера итерации (при необходимости)
+			/*
+			if (tau * c * 100 == 10.000000) {
+				nomer = nomer + 1;
+				ofstream NEV;
+				NEV.open("NEV1.txt");
+				NEV << setw(10) << NORMA << " ";
+
+				ofstream NOMu;
+				NOMu.open("NOMu1.txt");
+				NOMu << nomer << " ";
+				NEV.close();
+				NOMu.close();
+			}
+			*/
+
+			Norma = 0.0;
+			T4 = T2;
+			T2 = T3;
+			T3 = T4;
+		}
+
+		//вывод в файл основных значений(на определенных слоях)
+		int type = tau * c * 10;
+
+		if (((tau * c * 10 - type) == 0)) {
+			ofstream res;
+			res.open("res" + to_string(c * tau * 100) + ".txt");
+			res << setw(10) << " ";
+			for (int a = 0; a < N + 1; a++) {
+				for (int z = 0; z < N + 1; z++) {
+					res << setw(10) << T1[a][z] << " ";
+				}
+				res << endl;
+			}
+			res.close();
+		}
+
+		T4 = T2;
+		T2 = T1;
+		T1 = T4;
+		q = 0;
+		c++;
+		NORMA = 10.0;
+	}
+
+	//очищение массивов
+	for (int i = 0; i < N + 1; i++) {
+		delete[]T1[i];
+	}
+	for (int i = 0; i < N + 1; i++) {
+		delete[]T2[i];
+	}
+	for (int i = 0; i < N + 1; i++) {
+		delete[]T3[i];
+	}
+}
---
+++ Gauss_seidel/Gauss_seidel/plot.m
@@ -0,0 +1,345 @@
+clc;
+close all
+clear all;
+
+
+
+R1=dlmread('res10.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R1,0:0.1:1)
+hold on;
+pause(2)
+
+
+R2=dlmread('res20.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R2,0:0.1:1)
+hold on;
+pause(2)
+
+
+R3=dlmread('res30.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R3,0:0.1:1)
+hold on;
+pause(1)
+
+
+R4=dlmread('res40.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R4,0:0.1:1)
+hold on;
+pause(1)
+
+R5=dlmread('res50.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R5,0:0.1:1)
+hold on;
+pause(1)
+
+R6=dlmread('res60.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R6,0:0.1:1)
+hold on;
+pause(1)
+
+R8=dlmread('res80.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R8,0:0.1:1)
+hold on;
+pause(1)
+
+R9=dlmread('res90.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R9,0:0.1:1)
+hold on;
+pause(1)
+
+R10=dlmread('res100.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R10,0:0.1:1)
+hold on;
+pause(1)
+
+R11=dlmread('res110.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R11,0:0.1:1)
+hold on;
+pause(1)
+
+R12=dlmread('res120.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R12,0:0.1:1)
+hold on;
+pause(1)
+
+R13=dlmread('res130.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R13,0:0.1:1)
+hold on;
+pause(1)
+
+R15=dlmread('res150.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R15,0:0.1:1)
+hold on;
+pause(1)
+
+R16=dlmread('res160.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R16,0:0.1:1)
+hold on;
+pause(1)
+
+R17=dlmread('res170.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R17,0:0.1:1)
+hold on;
+pause(1)
+
+R18=dlmread('res180.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R18,0:0.1:1)
+hold on;
+pause(1)
+
+R19=dlmread('res190.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R19,0:0.1:1)
+hold on;
+pause(1)
+
+R20=dlmread('res200.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R20,0:0.1:1)
+hold on;
+pause(1)
+
+R21=dlmread('res210.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R21,0:0.1:1)
+hold on;
+pause(1)
+
+R22=dlmread('res220.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R22,0:0.1:1)
+hold on;
+pause(1)
+
+R24=dlmread('res240.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R24,0:0.1:1)
+hold on;
+pause(1)
+
+R25=dlmread('res250.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R25,0:0.1:1)
+hold on;
+pause(1)
+
+R26=dlmread('res260.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R26,0:0.1:1)
+hold on;
+pause(1)
+
+R27=dlmread('res270.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R27,0:0.1:1)
+hold on;
+pause(1)
+
+R30=dlmread('res300.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R30,0:0.1:1)
+hold on;
+pause(1)
+
+R31=dlmread('res310.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R31,0:0.1:1)
+hold on;
+pause(1)
+
+R32=dlmread('res320.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R32,0:0.1:1)
+hold on;
+pause(1)
+
+R33=dlmread('res330.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R33,0:0.1:1)
+hold on;
+pause(1)
+
+R34=dlmread('res340.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R34,0:0.1:1)
+hold on;
+pause(1)
+
+R35=dlmread('res350.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R35,0:0.1:1)
+hold on;
+pause(1)
+
+R36=dlmread('res360.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R36,0:0.1:1)
+hold on;
+pause(1)
+
+R37=dlmread('res370.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R37,0:0.1:1)
+hold on;
+pause(1)
+
+R38=dlmread('res380.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R38,0:0.1:1)
+hold on;
+pause(1)
+
+R39=dlmread('res390.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R39,0:0.1:1)
+hold on;
+pause(1)
+
+R40=dlmread('res400.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R40,0:0.1:1)
+hold on;
+pause(1)
+
+R42=dlmread('res420.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R42,0:0.1:1)
+hold on;
+pause(1)
+
+R43=dlmread('res430.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R43,0:0.1:1)
+hold on;
+pause(1)
+
+R44=dlmread('res440.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R44,0:0.1:1)
+hold on;
+pause(1)
+
+R45=dlmread('res450.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R45,0:0.1:1)
+hold on;
+pause(1)
+
+R47=dlmread('res470.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R47,0:0.1:1)
+hold on;
+pause(1)
+
+R48=dlmread('res480.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R48,0:0.1:1)
+hold on;
+pause(1)
+
+R49=dlmread('res490.000000.txt');
+x=0:0.1:1;
+y=0:0.1:1;
+[X,Y] = meshgrid(x,y)
+contourf(X,Y,R49,0:0.1:1)
+hold on;
+figure;
+
